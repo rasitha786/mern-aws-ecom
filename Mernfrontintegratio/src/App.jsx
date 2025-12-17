08df37a92f2ed32279aeaccc414a6fc6d1e0b4fa
RASITHA <rasithar92@gmail.com>
2025-12-17 15:27:00 +0530

    Update App.jsx

--- Mernfrontintegratio/src/App.jsx
+++ Mernfrontintegratio/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
                   <span className="text-white font-bold text-xl">A</span>
                 </div>
                 <span className="ml-3 text-xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-                  AkiraMart ❤️
+                  AkIRAMART ❤️
                 </span>
               </Link>
             </div>
